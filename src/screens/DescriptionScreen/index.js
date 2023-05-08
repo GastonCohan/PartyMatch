2369da2f13f27174f46fbf8db526ccc3ca5430de
GastonCohan <gastoncohan2@gmail.com>
2023-05-07 22:00:55 -0300

    agregando logica de la imagen de descripcion

--- src/screens/DescriptionScreen/index.js
+++ src/screens/DescriptionScreen/index.js
@@ -1,13 +1,37 @@
 import { View, Text, Image, Button } from 'react-native'
-import React from 'react'
-import kravi from '../../assets/kravi.png'
-import { Rating, AirbnbRating} from 'react-native-ratings';
+import React, { useEffect, useState } from 'react'
+import { AirbnbRating} from 'react-native-ratings';
 
 const EventDescription = ({navigation, route}) => {
   const { name } = route.params;
   const { rating } = route.params;
   const { direccion } = route.params;
-  console.log(direccion)
+  const {place_id} = route.params
+  const [urlImage, setUrlImage] = useState('https://www.elgrupoinformatico.com/static/Noticias/2020/07/google-maps-marcador-1200x675.jpg');
+
+  useEffect(() => {
+    PlacePhoto()
+  }, []);
+
+  const PlacePhoto = () => {
+    const placeId = place_id;
+    const API_KEY = "AIzaSyCsC9NCObVkBA4TWwRJZkIQXHyoT_3ELB4";
+
+    const fetchPhoto = async () => {
+      const photoResponse = await fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=photos&key=${API_KEY}`);
+      const photoData = await photoResponse?.json();
+      const photoRef = photoData?.result?.photos[0]?.photo_reference;
+      const photoUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=${API_KEY}`;
+      setUrlImage(photoUrl);
+
+      if (!photoUrl) {
+        return null; // o un componente de carga
+      }
+    }
+      fetchPhoto();
+  };
+
+
   return (
     <View style={{backgroundColor:'white', height:'100%'}}>
       <View style={{marginTop: '10%', alignItems:"center"}}>
@@ -25,10 +49,10 @@ const EventDescription = ({navigation, route}) => {
         </View>
       </View>
       <View style={{marginTop:'8%', alignItems:"center"}}>
-        <Image source={kravi} style={{height: 250, width:300}}/>
+        <Image source={{uri: urlImage}} style={{height: 250, width:300}}/>
       </View>
       <View style={{marginLeft: '12%', marginTop: '2%'}}>
-        <Text style={{color:'black', marginTop: '5%', fontSize: 16}}>Direccion: {direccion ? direccion : 'No info.'}</Text>
+        <Text style={{color:'black', marginTop: '5%', fontSize: 16}}>Direccion: {direccion ? direccion : 'Sin informacion'}</Text>
         <Text style={{color:'black', marginTop: '5%', fontSize: 16}}>Capacidad: 2200 Personas</Text>
         <Text style={{color:'black', marginTop: '5%', fontSize: 16}}>Horario: 1am / 7am</Text>
         <Text style={{color:'black', marginTop: '5%', fontSize: 16}}>Fecha: 27/12/22</Text>
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -6,142 +6,137 @@ import Geolocation from 'react-native-geolocation-service';
 import { Platform, PermissionsAndroid } from 'react-native';
 import Modal from "react-native-modal";
 
-    const HomeScreen = ({navigation}) => {
-    const [searchQuery, setSearchQuery] = React.useState('');
-    const [markers, setMarkers] = useState([])
-    const [event, setEvent] = useState('Busca un evento para asistir')
-    const [myLat, setMyLat] = useState(0)
-    const [myLng, setMyLng] = useState(0)
-    const [isModalVisible, setModalVisible] = useState(false);
-    const [eventName, setEventName] = useState('');
-    const [rating, setRating] = useState('');
-    const [direccion, setDireccion] = useState('');
+const HomeScreen = ({ navigation }) => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const [markers, setMarkers] = useState([])
+  const [event, setEvent] = useState('Busca un evento para asistir')
+  const [myLat, setMyLat] = useState(0)
+  const [myLng, setMyLng] = useState(0)
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [eventName, setEventName] = useState('');
+  const [rating, setRating] = useState('');
+  const [direccion, setDireccion] = useState('');
+  const [placeId2, setPlaceId] = useState('');
 
-    const goToDescriptionScreen = () => {
-      if(event === 'Busca un evento para asistir'){
-        setModalVisible(true)
-      } else {
-        navigation.navigate('Event', {name: eventName, rating: rating, direccion: direccion})
-      }
+  const goToDescriptionScreen = () => {
+    if (event === 'Busca un evento para asistir') {
+      setModalVisible(true)
+    } else {
+      navigation.navigate('Event', { name: eventName, rating: rating, direccion: direccion, place_id: placeId2 })
     }
+  }
 
-    const toggleModal = () => {
-      setModalVisible(!isModalVisible);
-    };
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
 
-    const onChangeSearch = query => setSearchQuery(query);
+  const onChangeSearch = query => setSearchQuery(query);
 
-    
-    useEffect(() => {
-      requestPermissions()
-      componentDidMount()
+  useEffect(() => {
+    requestPermissions()
+    componentDidMount()
+    fetchingData()
+  }, [myLat, myLng]);
+
+  const componentDidMount = () => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        setMyLat(position.coords.latitude);
+        setMyLng(position.coords.longitude);
+      },
+      (error) => {
+        console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
+  }
+
+  useEffect(() => {
+    setTimeout(function () {
       fetchingData()
-    }, [myLat, myLng]); 
-    
-    console.log(myLat,myLng)
+    }, 5000)
+  }, [myLat, myLng]);
 
-    const componentDidMount = () => {
-        Geolocation.getCurrentPosition(
-          (position) => {
-            setMyLat(position.coords.latitude);
-            setMyLng(position.coords.longitude);
-          },
-          (error) => {
-            console.log(error.code, error.message);
-          },
-          { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-          ); 
-      }
-      
-      useEffect(() => {
-       setTimeout(function() {
-         fetchingData()
-       }, 5000)
-      }, [myLat, myLng]); 
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${myLat},${myLng}&radius=10000&type=night_club&key=AIzaSyCsC9NCObVkBA4TWwRJZkIQXHyoT_3ELB4`
 
-    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${myLat},${myLng}&radius=10000&type=night_club&key=AIzaSyCsC9NCObVkBA4TWwRJZkIQXHyoT_3ELB4`
-    
-    const fetchingData = () => {
-      fetch(url)
+  const fetchingData = () => {
+    fetch(url)
       .then((response) => response.json())
       .then((JsonResponse) => {
-        console.log("jsonResponse", JsonResponse)
-        setMarkers(JsonResponse.results)
+        setMarkers(JsonResponse.results.filter(item => item.business_status !== 'CLOSED_TEMPORARILY'))
       }).catch((error) => {
         console.log('error', error)
       })
+  }
+
+  const requestPermissions = async () => {
+    if (Platform.OS === 'ios') {
+      const auth = await Geolocation.requestAuthorization("whenInUse");
+      // if(auth === "granted") {
+      console.log(auth)
+      // }
     }
 
-    const requestPermissions = async () => {
-      if (Platform.OS === 'ios') {
-        const auth = await Geolocation.requestAuthorization("whenInUse");
-        // if(auth === "granted") {
-           console.log(auth)
-        // }
-      }
-    
-      if (Platform.OS === 'android') {
-        const auth = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        );
-        if (auth === 'granted') {
-          componentDidMount()
-        }
+    if (Platform.OS === 'android') {
+      const auth = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+      if (auth === 'granted') {
+        componentDidMount()
       }
     }
-
-    console.log(markers)
+  }
 
   return (
-    <View style={{backgroundColor:'#2E336A', height: '100%'}}>
-      <View style={{justifyContent:'center', alignItems:'center', marginTop: '9%'}}>
-      <Text style={{fontSize: 25, color:"#fff", fontFamily:"Cochin"}}>PARTYMATCH</Text>
+    <View style={{ backgroundColor: '#2E336A', height: '100%' }}>
+      <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: '9%' }}>
+        <Text style={{ fontSize: 25, color: "#fff", fontFamily: "Cochin" }}>PARTYMATCH</Text>
       </View>
-      <View style={{marginTop: '5%', width:"100%", alignItems:"center"}}>
-      <Searchbar
-      placeholder="Buscar evento..."
-      onChangeText={onChangeSearch}
-      value={searchQuery}
-      style={{borderTopWidth: 1,width:"100%", alignItems:'center', borderColor: 'black',backgroundColor:'#fff'}}
-    />
+      <View style={{ marginTop: '5%', width: "100%", alignItems: "center" }}>
+        <Searchbar
+          placeholder="Buscar evento..."
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+          style={{ borderTopWidth: 1, width: "100%", alignItems: 'center', borderColor: 'black', backgroundColor: '#fff' }}
+        />
       </View>
-      <View style={{marginTop: '-0.5%', justifyContent:'center', alignItems:"center", height: '74%'}}>
+      <View style={{ marginTop: '-0.5%', justifyContent: 'center', alignItems: "center", height: '74%' }}>
         <MapView
-         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-         style={{height:'100%', width:"100%"}}
-         zoomEnabled={true} 
-         scrollEnabled={true} 
-         showsScale={true}
-         region={{
-         latitude: myLat,
-         longitude: myLng,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }} 
-     >
-      {markers.map((item, i) => 
-       <Marker onPress={() => setEvent('Asistir a ' + item.name) & setEventName(item.name) & setRating(item.rating) & setDireccion(item.vicinity)} key={i}
-       coordinate={{ latitude: item.geometry.location.lat, longitude: item.geometry.location.lng}}
-       title={item.name}>
-        </Marker>
-         )}
-      <Marker onPress={() => setEvent('Busca un evento para asistir')}
-      coordinate={{ latitude: myLat, longitude:  myLng}}
-      title='Mi ubicacion'>
-          <Image source={require('../../assets/geolocalizacion.png')} style={{height: 35, width:45 }} />
-      </Marker>
-      </MapView>
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={{ height: '100%', width: "100%" }}
+          zoomEnabled={true}
+          scrollEnabled={true}
+          showsScale={true}
+          region={{
+            latitude: myLat,
+            longitude: myLng,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+          {markers.map((item, i) =>
+            <Marker onPress={() => setEvent('Asistir a ' + item.name) & setEventName(item.name) & setRating(item.rating) & setDireccion(item.vicinity) & setPlaceId(item.place_id)} key={i}
+              coordinate={{ latitude: item.geometry.location.lat, longitude: item.geometry.location.lng }}
+              title={item.name}>
+            </Marker>
+          )}
+          <Marker onPress={() => setEvent('Busca un evento para asistir')}
+            coordinate={{ latitude: myLat, longitude: myLng }}
+            title='Mi ubicacion'>
+            <Image source={require('../../assets/geolocalizacion.png')} style={{ height: 35, width: 45 }} />
+          </Marker>
+        </MapView>
       </View>
-      <View style={{marginTop: '0%', width:"100%"}}>
-      <Button title={event} onPress={() => goToDescriptionScreen()} color="#2E336A" style={{width:"100%"}}/>
+      <View style={{ marginTop: '0%', width: "100%" }}>
+        <Button title={event} onPress={() => goToDescriptionScreen()} color="#2E336A" style={{ width: "100%" }} />
       </View>
       <Modal isVisible={isModalVisible}>
-        <View style={{backgroundColor:"white", borderRadius: 5}}>
-          <View style={{height:80,justifyContent:"center", alignItems:'center'}}>
-          <Text style={{color:'black'}}>Debes seleccionar algun evento al que asistir</Text>
+        <View style={{ backgroundColor: "white", borderRadius: 5 }}>
+          <View style={{ height: 80, justifyContent: "center", alignItems: 'center' }}>
+            <Text style={{ color: 'black' }}>Debes seleccionar algun evento al que asistir</Text>
           </View>
-          <View style={{width:'100%'}}>
-          <Button title="Cerrar" onPress={toggleModal} color="#2E336A"/>
+          <View style={{ width: '100%' }}>
+            <Button title="Cerrar" onPress={toggleModal} color="#2E336A" />
           </View>
         </View>
       </Modal>
